c79bb8c7b849fd2f63fe63f8f1f62d98d3da1a40
谢冰 <xiebbing@126.com>
2019-06-11 13:53:37 +0800

    ADD LOG

---
+++ General.Log/Entity/LogEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace General.Log.Entity
+{
+    /// <summary>
+    /// 日志实体
+    /// </summary>
+    [Serializable]
+    public class LogEntity
+    {
+        /// <summary>
+        /// 日志类型
+        /// </summary>
+        public LogType LogType { get; set; }
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        public string ModelName { get; set; }
+        /// <summary>
+        /// 日志信息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        public string Exception { get; set; }
+        /// <summary>
+        /// 用户id
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime LogDate { get; set; }
+        /// <summary>
+        /// ip
+        /// </summary>
+        public string Ip { get; set; }
+    }
+    /// <summary>
+    /// 日志类型
+    /// </summary>
+    public enum LogType
+    {
+        Info = 1,
+        Debug = 2,
+        Warn = 3,
+        Error = 4,
+        Fatal = 5
+    }
+}
---
+++ General.Log/LogManager.cs
@@ -0,0 +1,52 @@
+﻿using log4net;
+using log4net.Repository;
+
+namespace General.Log
+{
+    /// <summary>
+    /// log manager
+    /// </summary>
+    public class LogManager : ILogManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly ILog _log = LogContext.Log;
+        /// <summary>
+        /// 
+        /// </summary>
+        //private readonly ILog _dbLog = LogContext.DbLog;
+        /// <summary>
+        /// 记录信息
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="logManagerType"></param>
+        public void Info(object message, LogManagerType logManagerType = LogManagerType.File)
+        {
+            _log.Info(message);
+            //if (logManagerType == LogManagerType.File)
+            //    _log.Info(message);
+            //else
+            //    _dbLog.Info(message);
+        }
+        /// <summary>
+        /// 记录debug
+        /// </summary>
+        /// <param name="message"></param>
+        public void Debug(object message)
+        {
+            if (_log.IsDebugEnabled)
+            {
+                _log.Debug(message);
+            }
+        }
+        /// <summary>
+        /// 记录错误信息
+        /// </summary>
+        /// <param name="message"></param>
+        public void Error(object message)
+        {
+            _log.Error(message);
+        }
+    }
+}
